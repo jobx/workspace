1e6f0123b2240bd8dbda1647dd1ac40ff8790cf5
jobx <34254893+jobx@users.noreply.github.com>
2018-05-22 23:07:49 -0500

    5/22/2018

--- Wolfram/Test/Test.m
+++ Wolfram/Test/Test.m
@@ -3,6 +3,8 @@
 BeginPackage["Test`"]
 
 fun::usage = "fun[x] computes x^2"
+ED::usage = "edit distance"
+Kn::usage = "Knapsack with replacement"
 
 Begin["`Private`"]
 
@@ -12,6 +14,43 @@ fun[x_]:= Module[
 	y
 ];
 
+Clear[ED];
+(* initialization *)
+ED[i_, 0] :=
+    i;
+ED[0, j_] :=
+    j;
+x = "polynomial";
+y = "exponential";
+ED[i_, j_] :=
+    ED[i, j] = Module[ {c1, c2, c3, best},
+                   c1 = If[ Characters[x][[i]] == Characters[y][[j]],
+                            0,
+                            1
+                        ] + 
+                     ED[i - 1, j - 1];
+                   c2 = 1 + ED[i, j - 1];
+                   c3 = 1 + ED[i - 1, j];
+                   best = Min[c1, c2, c3];
+                   Switch[Position[{c1, c2, c3}, best] // Flatten // First,
+                    1, xr[i] = Characters[x][[i]];
+                       yr[j] = Characters[y][[j]],
+                    2, xr[i] = "-",
+                    3, yr[j] = "-"];
+                   best
+               ];
+               
+Kn[0] = 0;
+Kn[w_] :=
+    Kn[w] = Module[ {i, choices, best},
+                i = Select[item, #[[1]] <= w &];
+                If[ Length[i] == 0,
+                    0,
+                    choices = i /. {a_Integer, b_Integer} -> Kn[w - a] + b;
+                    best = Max[choices]
+                ]
+            ];
+                   
 End[]
 
 EndPackage[]
